60409d47be0f56408d7d8591f4a1cbed19ab40ec
davidicus <david@david-conner.com>
2023-06-22 09:37:02 -0400

    feat(build): tweak build for individual packages and types

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,42 @@
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 import react from '@vitejs/plugin-react'
 import copy from 'rollup-plugin-copy';
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import * as packageJson from './package.json'
 
+const plugins = [
+  react(),
+  dts(),
+]
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: plugins,
   build: {
+    minify: true,
+    reportCompressedSize: true,
     lib: {
       // Could also be a dictionary or array of multiple entry points
       entry: ['src/index.ts'],
       name: 'Seldon',
-      formats: ['es', 'umd'],
-      // the proper extensions will be added
-      fileName: 'seldon',
+      formats: ['es'],
     },
+    outDir: 'dist',
     rollupOptions: {
+      input: 'src/index.ts',
+      output: [
+        {
+          dir: 'dist',
+          preserveModulesRoot: 'src',
+          chunkFileNames: '[name].js',
+          entryFileNames: '[name].js'
+        }
+      ],
+      preserveModules: true,
       // make sure to externalize deps that shouldn't be bundled
       // into your library
-      external: ['react, react-dom'],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {
-          react: 'React',
-          'react-dom': 'ReactDOM',
-        },
-      },
+      external: [...Object.keys(packageJson.peerDependencies)],
       plugins: [
         copy({
           flatten: false,
