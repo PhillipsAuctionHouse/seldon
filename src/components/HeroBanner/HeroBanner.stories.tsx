a688ed444454f1cb973ac2851d1f52aa53311de1
davidicus <david@david-conner.com>
2023-07-05 14:30:47 -0400

    feat(herobanner): add conventions discussed during code review

--- src/components/HeroBanner/HeroBanner.stories.tsx
+++ src/components/HeroBanner/HeroBanner.stories.tsx
@@ -7,9 +7,6 @@ const meta = {
   title: 'Components/HeroBanner',
   component: HeroBanner,
   tags: ['autodocs'],
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } satisfies Meta<typeof HeroBanner>;
 
 export default meta;
@@ -18,11 +15,11 @@ type Story = StoryObj<typeof meta>;
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
 export const Playground: Story = {
   args: {
-    region: "New York",
+    prehead: "New York",
     date: "15 NOV 2023",
     headerText: "20th Century & Contemporary Art",
     subHeadText: "Day Sales — Morning Session",
     association: "In Association with BACS & RUSSO",
-    backgroundImage: "https://assets.phillips.com/image/upload/t_Website_DepartmentHero/v1685035132/website/department/hero-12.jpg"
+    background: "linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)), url(https://assets.phillips.com/image/upload/t_Website_DepartmentHero/v1685035132/website/department/hero-12.jpg)"
   },
 };
--- src/components/HeroBanner/HeroBanner.tsx
+++ src/components/HeroBanner/HeroBanner.tsx
@@ -4,9 +4,9 @@ import { px } from '../../utils';
 
 interface HeroBannerProps {
   /**
-   * Where
+   * informational text above the header (e.g. region label or "buy now")
    */
-  region?: string ;
+  prehead?: string ;
   /**
    * When is this sale taking place?
    */
@@ -14,7 +14,7 @@ interface HeroBannerProps {
   /**
    * Main header text
    */
-  headerText?: string;
+  headerText: string;
   /**
    * Sub-head text
    */
@@ -24,61 +24,55 @@ interface HeroBannerProps {
   */
   association?: string;
   /**
-  * What background color to use
+  * What background image or color to use
   */
-  backgroundColor?: string;
-  /**
-  * What background image to use
-  */
-  backgroundImage?: string;
+  background?: string;
   /**
   * Unique id for component
   */
-  id?: string ; 
+  id?: string ;
 }
 
 /**
  * Hero Banner UI component
  */
 
+const baseClass = `${px}-hero-banner`;
+
 const HeroBanner = ({
-  region,
+  prehead,
   date,
   headerText,
   subHeadText,
   association,
-  backgroundColor,
-  backgroundImage,
+  background,
   id
 }: HeroBannerProps) => {
   return (
-    <div
-      data-testid={id ? `hero-banner__${id}` : `heroBanner`}
-      className={classnames(`${px}-hero-banner`)}
-      style={{ backgroundColor, backgroundImage: `linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5)), url(${backgroundImage})` }}
+    <header
+      data-testid={id ? `hero-banner-${id}` : `hero-banner`}
+      className={baseClass}
+      style={{ '--background': background } as React.CSSProperties}
     >
-      <div className={classnames(`${px}-hero-banner__text-overlay`)}>
-        {(region || date) && 
-        <div className={classnames(`${px}-hero-banner__text-overlay__pre-head`)}>
-          {region && <div className={classnames(`${px}-hero-banner__text-overlay__pre-head__region`)}>{region}</div>}
-          {date && <div className={classnames(`${px}-hero-banner__text-overlay__pre-head__date`)}>{date}</div>}
-        </div>}
-        {headerText && 
-        <div className={classnames(`${px}-hero-banner__text-overlay__heading`)}>
-          <div className={classnames(`${px}-hero-banner__text-overlay__heading__header`)}>
-            <h1>{headerText}</h1>
+      {
+        (prehead || date)
+        ? <p className={`${baseClass}__pre-head`}>
+            {prehead ? <span>{prehead}</span> : null}
+            {date ? <span>{date}</span> : null}
+          </p>
+        : null
+      }
+      {
+        headerText
+        ? <div className={`${baseClass}__heading`}>
+            <h1>{headerText}{ subHeadText ? <span>{subHeadText}</span> : null }</h1>
           </div>
-          {subHeadText && 
-          <div className={classnames(`${px}-hero-banner__text-overlay__heading__sub-head`)}>
-            <h2>{subHeadText}</h2>
-          </div>}
-        </div>}
-        {association && 
-        <div className={classnames(`${px}-hero-banner__text-overlay__association`)}>
-          <p>{association}</p>
-        </div>}
-      </div>
-    </div>
+        : null
+      }
+      {
+        association ? <p>{association}</p> : null
+      }
+    </header>
   );
 };
 
