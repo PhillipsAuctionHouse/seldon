dfe3e7977815fc85ab3651d008defe23fc36af4b
davidicus <david@david-conner.com>
2023-09-21 20:51:49 -0400

    feat(input): styled different states

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as Button } from './components/Button/Button';
 export { default as Header } from './components/Header/Header';
 export { default as HeroBanner } from './components/HeroBanner/HeroBanner';
+export { default as Input } from './components/Input/Input';
 export { default as Page } from './pages/Page';
