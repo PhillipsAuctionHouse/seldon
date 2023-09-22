8b543ff3cbc5badf6db5491f2d495e3d0905e32a
davidicus <david@david-conner.com>
2023-09-22 19:05:32 -0400

    chore(inputs): add select to export

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export { default as Button } from './components/Button/Button';
 export { default as Header } from './components/Header/Header';
 export { default as HeroBanner } from './components/HeroBanner/HeroBanner';
 export { default as Input } from './components/Input/Input';
+export { default as Select } from './components/Select/Select';
 export { default as Page } from './pages/Page';
