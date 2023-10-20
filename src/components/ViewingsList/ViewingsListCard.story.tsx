c950f19b874d2f7347b3f9f7b3d99b6a15e1aa90
davidicus <david@david-conner.com>
2023-10-20 14:28:14 -0400

    feat(viewingscard): first pass

---
+++ src/components/ViewingsList/ViewingsListCard.story.tsx
@@ -0,0 +1,38 @@
+import type { Meta } from '@storybook/react';
+
+import Button, { ButtonProps } from './Button';
+import Calendar from '../../assets/calendar.svg?react';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  tags: ['autodocs'],
+  argTypes: {
+    size: {
+      options: ['sm', 'md', 'lg'],
+      control: {
+        type: 'select',
+      },
+    },
+    iconLast: { control: 'boolean' },
+  },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+
+export const ButtonWithIcon = (props: ButtonProps) => (
+  <Button {...props}>
+    {!props.iconLast ? <Calendar /> : null}
+    This is the title
+    {props.iconLast ? <Calendar /> : null}
+  </Button>
+);
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+export const Playground = {
+  args: {
+    primary: false,
+    children: 'Button',
+  },
+};
---
+++ src/components/ViewingsList/ViewingsListCard.tsx
@@ -0,0 +1,59 @@
+import classnames from 'classnames';
+
+import { px } from '../../utils';
+
+export interface ViewingsListCardProps {
+  /**
+   * Is this the principal call to action on the page?
+   */
+  primary?: boolean ;
+  /**
+   * What background color to use
+   */
+  backgroundColor?: string;
+  /**
+   * How large should the button be?
+   */
+  size?: 'sm' | 'md' | 'lg';
+
+  /**
+   * Button contents
+   */
+  children: React.ReactNode
+  /**
+  * Unique id for component
+  */
+  id?: string ;
+  /**
+   * Optional click handler
+   */
+  onClick?: () => void;
+}
+
+/**
+ * Primary UI component for user interaction
+ */
+
+const ViewingsListCard = ({
+  primary = true,
+  size = 'md',
+  backgroundColor,
+  children,
+  id,
+  ...props
+}: ViewingsListCardProps) => {
+  return (
+    <button
+      data-testid={id ? `button-${id}` : `button`}
+      id={id}
+      type="button"
+      className={classnames(`${px}-button`,`${px}-button--${size}`, {[`${px}-button--secondary`]: !primary})}
+      style={{ backgroundColor }}
+      {...props}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default ViewingsListCard
