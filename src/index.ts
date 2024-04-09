a93590a4bff7fe58c1ab96ef4f6744355373ece1
davidicus <david@david-conner.com>
2024-04-09 01:22:17 -0400

    feat: add Grid to input export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
 export { default as Button } from './components/Button/Button';
 export { default as ErrorBoundary } from './components/ErrorBoundary/ErrorBoundary';
 // export { default as DatePicker } from './components/DatePicker/DatePicker';
+export { default as Grid } from './components/Grid/Grid';
 export { default as Header } from './components/Header/Header';
 export { default as HeroBanner } from './components/HeroBanner/HeroBanner';
 export { default as Input } from './components/Input/Input';
