c8d9ee6a021378dfa5749a7b0573d16cded96ead
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-04-18 15:13:45 -0500

    chore(stylelint.config): update the stylelint config to match remix repo

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,8 +1,24 @@
 module.exports = {
-  extends: ['stylelint-config-recommended', 'stylelint-config-recommended-scss'],
-  plugins: ['stylelint-scss'],
+  extends: ['stylelint-config-standard', 'stylelint-config-standard-scss'],
+  plugins: ['stylelint-scss', 'stylelint-order'],
   rules: {
     'at-rule-no-unknown': null,
     'scss/at-rule-no-unknown': true,
+    'order/properties-alphabetical-order': true,
+
+    // Relax optionated naming conventions
+    'scss/dollar-variable-pattern': null,
+    'scss/at-mixin-pattern': null,
+    'selector-class-pattern': [
+      '^[a-z]([-]?[a-z0-9]+)*(__[a-z0-9]([-]?[a-z0-9]+)*)?(--[a-z0-9]([-]?[a-z0-9]+)*)?$',
+      {
+        /** This option will resolve nested selectors with & interpolation. - https://stylelint.io/user-guide/rules/selector-class-pattern/#resolvenestedselectors-true--false-default-false */
+        resolveNestedSelectors: true,
+        /** Custom message */
+        message: function expected(selectorValue) {
+          return `Expected class selector "${selectorValue}" to match BEM CSS pattern https://en.bem.info/methodology/css. Selector validation tool: https://regexr.com/3apms`;
+        },
+      },
+    ],
   },
 };
