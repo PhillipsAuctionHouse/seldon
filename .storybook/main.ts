8dcd55fc44fd34aa2cb34329f0aa8954e5d1cf67
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-05-23 11:28:37 -0500

    chore(storybook): fix typescript issues

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -6,7 +6,6 @@ const config: StorybookConfig = {
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
-    "@storybook/addon-styling",
     "@storybook/addon-mdx-gfm",
     "@chromatic-com/storybook"
   ],
