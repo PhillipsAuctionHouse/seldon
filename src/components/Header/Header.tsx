8e0f71442f601d21e86a8d41c45e9e3ea8d7c847
PHILLIPS\ADietrich <adietrich@phillips.com>
2024-06-03 12:17:17 -0400

    feat(navigationitem): add navigation item

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
 import classnames from 'classnames';
 import * as React from 'react';
 import { px } from '../../utils';
-import Navigation from '../Navigation/Navigation';
-import NavigationItem from '../Navigation/NavigationItem/NavigationItem';
-import NavigationItemTrigger from '../Navigation/NavigationItemTrigger/NavigationItemTrigger';
-import NavigationList from '../Navigation/NavigationList/NavigationList';
+// import Navigation from '../Navigation/Navigation';
+// import NavigationItem from '../Navigation/NavigationItem/NavigationItem';
+// import NavigationItemTrigger from '../Navigation/NavigationItemTrigger/NavigationItemTrigger';
+// import NavigationList from '../Navigation/NavigationList/NavigationList';
 import Search from '../Search/Search';
-import { LinkVariants } from '../Link/utils';
+// import { LinkVariants } from '../Link/utils';
 
 export interface HeaderProps extends React.HTMLAttributes<HTMLElement> {
   /**
@@ -26,12 +26,28 @@ export interface HeaderProps extends React.HTMLAttributes<HTMLElement> {
    */
   toggleCloseText?: string;
 }
+type HeaderContextType = {
+  expandedItem: string;
+  expanded: boolean;
+  handleSelection: (label: string) => void;
+  handleFocusEvent: (e: React.FocusEvent) => void;
+};
 
 const defaultMobileMenuLabel = 'Main Menu';
+export const HeaderContext = React.createContext({
+  expandedItem: defaultMobileMenuLabel,
+  expanded: false,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+  handleSelection: (_label: string) => {},
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+  handleFocusEvent: (_e: React.FocusEvent) => {},
+});
+
 const Header = ({
   logo,
   logoText,
   className,
+  children,
   toggleOpenText = 'Open Menu',
   toggleCloseText = 'Close Menu',
 }: React.PropsWithChildren<HeaderProps>) => {
@@ -72,358 +88,11 @@ const Header = ({
         <img src={logo} height="14" alt={logoText} />
       </h1>
       <div className={classnames(`${px}-header__nav`, { [`${px}-header__nav--open`]: toggleState })}>
-        <Navigation
-          id={`${px}-main-nav`}
-          label={expandedItem}
-          backBtnLabel="← Back"
-          onBack={() => setExpandedItem(defaultMobileMenuLabel)}
-          expanded={expanded}
+        <HeaderContext.Provider
+          value={{ expandedItem, expanded, handleSelection, handleFocusEvent } as HeaderContextType}
         >
-          <NavigationList>
-            <NavigationItemTrigger
-              label={`Auctions`}
-              expandedItem={expandedItem}
-              handleOnClick={() => handleSelection('Auctions')}
-            >
-              <NavigationList>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`Editions & Works on Paper`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>New York</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`Casa Fornaroli`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>London</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`The Geneva Watch Auction: XVII`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>Geneva</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`Modern & Contemporary Art Day Sale—Morning Session`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>New York</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`Modern & Contemporary Art Day Sale—Afternoon Session`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>New York</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`Modern & Contemporary Art Evening Sale`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>New York</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`Wired: Online Auction`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>London</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`The Imperial Patek Philippe Sale`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>Hong Kong</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`Disruptors: Evening Sale of Modern & Contemporary Art, Design and Watches`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>Hong Kong</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Browse Full Auction Calendar`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Auction Calendar`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Auction Results`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Artists & Makers`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`How To Buy`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Remote Bidding`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-              </NavigationList>
-            </NavigationItemTrigger>
-            <NavigationItem href="#" label={`Calendar`} />
-            <NavigationItemTrigger
-              label={`Departments`}
-              expandedItem={expandedItem}
-              handleOnClick={() => handleSelection('Departments')}
-            >
-              <NavigationList>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Modern & Contemporary Art`}
-                  tabIndex={expandedItem === 'Departments' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Design`}
-                  tabIndex={expandedItem === 'Departments' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Editions`}
-                  tabIndex={expandedItem === 'Departments' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Jewels`}
-                  tabIndex={expandedItem === 'Departments' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Photographs`}
-                  tabIndex={expandedItem === 'Departments' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Watches`}
-                  tabIndex={expandedItem === 'Departments' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  label={`Private Sales`}
-                  tabIndex={expandedItem === 'Departments' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                />
-              </NavigationList>
-            </NavigationItemTrigger>
-            <NavigationItemTrigger
-              label={`Exhibitions`}
-              expandedItem={expandedItem}
-              handleOnClick={() => handleSelection('Exhibitions')}
-            >
-              <NavigationList>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`Written in the Sky: Works by Ed Ruscha`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>New York</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`Modeler le papier // Shapes On Paper`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>Paris</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`ALT POP: An Alternative History to American Pop Art`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>New York</span>
-                </NavigationItem>
-                <NavigationItem
-                  href="#"
-                  navGroup={'nav-link-lg'}
-                  navType={LinkVariants.navLinkLg}
-                  label={`New Terrains: Contemporary Native American Art`}
-                  tabIndex={expandedItem === 'Auctions' ? 0 : -1}
-                  onBlur={handleFocusEvent}
-                >
-                  <span className={`${px}-nav__item--badge `}>New York</span>
-                </NavigationItem>
-
-                <NavigationItem
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  href="#"
-                  label={`Past Exhibitions`}
-                />
-              </NavigationList>
-            </NavigationItemTrigger>
-            <NavigationItem href="#" label={`Perpetual`} />
-            <NavigationItem href="#" label={`Dropshop`} />
-            <NavigationItemTrigger
-              label={`Buy & Sell`}
-              expandedItem={expandedItem}
-              handleOnClick={() => handleSelection('Buy & Sell')}
-            >
-              <NavigationList>
-                <NavigationItem
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  href="#"
-                  label={`How To Buy`}
-                />
-                <NavigationItem
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  href="#"
-                  label={`How To Sell`}
-                />
-                <NavigationItem
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  href="#"
-                  label={`Remote Bidding`}
-                />
-                <NavigationItem
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  href="#"
-                  label={`Private Services`}
-                />
-                <NavigationItem
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  href="#"
-                  label={`Trusts, Estates & Valuations`}
-                />
-                <NavigationItem
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  href="#"
-                  label={`Fiduciary Services`}
-                />
-                <NavigationItem
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  href="#"
-                  label={`Buy Catalogues`}
-                />
-              </NavigationList>
-            </NavigationItemTrigger>
-            <NavigationItem href="#" label={`Editorial`} />
-            <NavigationItemTrigger
-              label={`About Us`}
-              expandedItem={expandedItem}
-              handleOnClick={() => handleSelection('About Us')}
-            >
-              <NavigationList>
-                <NavigationItem
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  href="#"
-                  label={`Our History`}
-                />
-                <NavigationItem navGroup={'nav-link-sm'} navType={LinkVariants.navLinkSm} href="#" label={`Our Team`} />
-                <NavigationItem
-                  navGroup={'nav-link-sm'}
-                  navType={LinkVariants.navLinkSm}
-                  href="#"
-                  label={`Locations`}
-                />
-                <NavigationItem navGroup={'nav-link-sm'} navType={LinkVariants.navLinkSm} href="#" label={`Press`} />
-                <NavigationItem navGroup={'nav-link-sm'} navType={LinkVariants.navLinkSm} href="#" label={`Careers`} />
-              </NavigationList>
-            </NavigationItemTrigger>
-          </NavigationList>
-        </Navigation>
+          {children}
+        </HeaderContext.Provider>
       </div>
       <Search />
     </header>
--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,10 @@
 import * as React from 'react';
 import { px } from '../../utils';
 import classNames from 'classnames';
+import { HeaderContext } from '../Header/Header';
 
 export interface NavigationProps extends React.HTMLAttributes<HTMLElement> {
   backBtnLabel?: string;
-  /**
-   * Optional expanded state
-   */
-  expanded?: boolean;
   /**
    * Optional label
    */
@@ -26,30 +23,32 @@ const Navigation = ({
   backBtnLabel,
   children,
   className,
-  expanded,
   id,
   label = 'Main Menu',
   onBack,
   visible = true,
-}: React.PropsWithChildren<NavigationProps>) => (
-  <nav
-    aria-labelledby={`${id}-label`}
-    data-testid={id ? `nav-${id}` : `nav`}
-    id={id}
-    className={classNames(`${px}-nav`, className, { [`${px}-nav--expanded`]: expanded })}
-  >
-    <button tabIndex={expanded ? 0 : -1} className={`${px}-nav__back-btn`} onClick={onBack}>
-      {backBtnLabel}
-    </button>
-    <h2
-      id={`${id}-label`}
-      className={`${px}-nav__label`}
-      style={{ '--visible': visible ? 'visible' : 'hidden' } as React.CSSProperties}
+}: React.PropsWithChildren<NavigationProps>) => {
+  const { expanded } = React.useContext(HeaderContext);
+  return (
+    <nav
+      aria-labelledby={`${id}-label`}
+      data-testid={id ? `nav-${id}` : `nav`}
+      id={id}
+      className={classNames(`${px}-nav`, className, { [`${px}-nav--expanded`]: expanded })}
     >
-      {label}
-    </h2>
-    {children}
-  </nav>
-);
+      <button tabIndex={expanded ? 0 : -1} className={`${px}-nav__back-btn`} onClick={onBack}>
+        {backBtnLabel}
+      </button>
+      <h2
+        id={`${id}-label`}
+        className={`${px}-nav__label`}
+        style={{ '--visible': visible ? 'visible' : 'hidden' } as React.CSSProperties}
+      >
+        {label}
+      </h2>
+      {children}
+    </nav>
+  );
+};
 
 export default Navigation;
