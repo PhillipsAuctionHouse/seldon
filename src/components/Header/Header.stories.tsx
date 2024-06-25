644229dc7ea41645862b759c84459e1c28483c84
PHILLIPS\ADietrich <adietrich@phillips.com>
2024-06-25 16:33:13 -0400

    chore(header): L3-376 style fixes for clicks in open areas triggering nav item clicks

--- src/components/Header/Header.stories.tsx
+++ src/components/Header/Header.stories.tsx
@@ -12,10 +12,16 @@ import UserManagement from '../UserManagement/UserManagement';
 const meta = {
   title: 'Components/Header',
   component: Header,
-  tags: ['autodocs'],
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
+    docs: {
+      story: {
+        height: '700px',
+      },
+    },
     layout: 'fullscreen',
+    viewport: {
+      defaultViewport: 'mobile1',
+    },
   },
 } satisfies Meta<typeof Header>;
 
