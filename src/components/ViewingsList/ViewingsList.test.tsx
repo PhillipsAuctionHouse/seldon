3b604ea450fc37011e8896776f85487ec8176e5e
Kostiantyn Huzenko <huzenko.c@gmail.com>
2024-07-03 09:00:28 -0400

    fix(testing): fixed test with long timeout

--- src/components/ViewingsList/ViewingsList.test.tsx
+++ src/components/ViewingsList/ViewingsList.test.tsx
@@ -5,6 +5,7 @@ import StatefulViewingsList from './StatefulViewingsList';
 import { defaultViewing, i18n, validate, handleOnSave } from './utils';
 import { ViewingsListCardProps } from './ViewingsListCard';
 import { runCommonTests } from '../../utils/testUtils';
+import { INCREASED_TEST_TIMEOUT } from '../../utils/constants';
 
 describe('ViewingsList', () => {
   const reqProps = { title: 'Tour Viewing(s) on Overview Tab', id: 'test-id', validate, onSave: handleOnSave };
@@ -86,89 +87,93 @@ describe('ViewingsList', () => {
     expect(screen.queryByDisplayValue(/New location/)).not.toBeInTheDocument();
   });
 
-  it('will save all values of the form and return them as part of the "handleOnSave" callback', async () => {
-    const user = userEvent.setup();
-    const mockedHandleOnSave = vi.fn().mockImplementation((e, cb) => {
-      const targ = e?.target as HTMLElement;
-      const inputs = targ.closest('.phillips-viewings-list-card')?.querySelectorAll('input');
+  it(
+    'will save all values of the form and return them as part of the "handleOnSave" callback',
+    async () => {
+      const user = userEvent.setup();
+      const mockedHandleOnSave = vi.fn().mockImplementation((e, cb) => {
+        const targ = e?.target as HTMLElement;
+        const inputs = targ.closest('.phillips-viewings-list-card')?.querySelectorAll('input');
 
-      const el: ViewingsListCardProps = { id: '' };
-      inputs?.forEach((input) => {
-        el[input.name] = input.value;
+        const el: ViewingsListCardProps = { id: '' };
+        inputs?.forEach((input) => {
+          el[input.name] = input.value;
+        });
+        cb((prevViewings: ViewingsListCardProps[]) => {
+          let returnValue: ViewingsListCardProps[] = [];
+          if (prevViewings) {
+            const unique = prevViewings.filter((view) => {
+              return view?.id !== el.id;
+            });
+            returnValue = [...unique, el as ViewingsListCardProps];
+          } else {
+            returnValue = [el as ViewingsListCardProps];
+          }
+
+          return returnValue;
+        });
+        return el;
       });
-      cb((prevViewings: ViewingsListCardProps[]) => {
-        let returnValue: ViewingsListCardProps[] = [];
-        if (prevViewings) {
-          const unique = prevViewings.filter((view) => {
-            return view?.id !== el.id;
-          });
-          returnValue = [...unique, el as ViewingsListCardProps];
-        } else {
-          returnValue = [el as ViewingsListCardProps];
-        }
+      render(<StatefulViewingsList title="Tour Viewing(s) on Overview Tab" id="test-id" onSave={mockedHandleOnSave} />);
+      await user.click(screen.getByText(/ADD VIEWING/));
 
-        return returnValue;
-      });
-      return el;
-    });
-    render(<StatefulViewingsList title="Tour Viewing(s) on Overview Tab" id="test-id" onSave={mockedHandleOnSave} />);
-    await user.click(screen.getByText(/ADD VIEWING/));
+      await user.keyboard('One title');
+      await user.click(screen.getByTestId(/^(previewOn-)[a-zA-z0-9]+/).previousSibling as Element);
+      await user.click(screen.getByTestId(/^(previewLabel-)[a-zA-z0-9]+/));
+      await user.keyboard('Opening Night');
+      await user.click(screen.getByTestId(/^(previewDates-)[a-zA-z0-9]+/));
+      await user.keyboard('3-6 November');
+      await user.click(screen.getByTestId(/^(previewHours1-)[a-zA-z0-9]+/));
+      await user.keyboard('8am - 9pm');
+      await user.click(screen.getByTestId(/^(previewHours2-)[a-zA-z0-9]+/));
+      await user.keyboard('After closing');
+      await user.click(screen.getByTestId(/^(viewingLabel-)[a-zA-z0-9]+/));
+      await user.keyboard('Open to the public');
+      await user.click(screen.getByTestId(/^(viewingDates-)[a-zA-z0-9]+/));
+      await user.keyboard('3-6 December');
+      await user.click(screen.getByTestId(/^(viewingHours1-)[a-zA-z0-9]+/));
+      await user.keyboard('7am - 8pm');
+      await user.click(screen.getByTestId(/^(viewingHours2-)[a-zA-z0-9]+/));
+      await user.keyboard('9:30pm - 10pm');
+      await user.click(screen.getByTestId(/^(address1-)[a-zA-z0-9]+/));
+      await user.keyboard('432 PARK AVE');
+      await user.click(screen.getByTestId(/^(address2-)[a-zA-z0-9]+/));
+      await user.keyboard('Coral Springs, FL 07677');
+      await user.click(screen.getByTestId(/^(address3-)[a-zA-z0-9]+/));
+      await user.keyboard('United States');
+      await user.click(screen.getByTestId(/^(addressUrl-)[a-zA-z0-9]+/));
+      await user.keyboard('HTTP://WWW.WEBSITE.COM');
+      await user.click(screen.getByTestId(/^(emailOn-)[a-zA-z0-9]+/));
+      await user.click(screen.getByTestId(/^(email-)[a-zA-z0-9]+/));
+      await user.keyboard('contact');
+      await user.click(screen.getByTestId(/^(emailLink-)[a-zA-z0-9]+/));
+      await user.keyboard('contact@phillips.com');
+      await user.click(screen.getByTestId(/^(enableOnSite-)[a-zA-z0-9]+/).previousSibling as Element);
 
-    await user.keyboard('One title');
-    await user.click(screen.getByTestId(/^(previewOn-)[a-zA-z0-9]+/).previousSibling as Element);
-    await user.click(screen.getByTestId(/^(previewLabel-)[a-zA-z0-9]+/));
-    await user.keyboard('Opening Night');
-    await user.click(screen.getByTestId(/^(previewDates-)[a-zA-z0-9]+/));
-    await user.keyboard('3-6 November');
-    await user.click(screen.getByTestId(/^(previewHours1-)[a-zA-z0-9]+/));
-    await user.keyboard('8am - 9pm');
-    await user.click(screen.getByTestId(/^(previewHours2-)[a-zA-z0-9]+/));
-    await user.keyboard('After closing');
-    await user.click(screen.getByTestId(/^(viewingLabel-)[a-zA-z0-9]+/));
-    await user.keyboard('Open to the public');
-    await user.click(screen.getByTestId(/^(viewingDates-)[a-zA-z0-9]+/));
-    await user.keyboard('3-6 December');
-    await user.click(screen.getByTestId(/^(viewingHours1-)[a-zA-z0-9]+/));
-    await user.keyboard('7am - 8pm');
-    await user.click(screen.getByTestId(/^(viewingHours2-)[a-zA-z0-9]+/));
-    await user.keyboard('9:30pm - 10pm');
-    await user.click(screen.getByTestId(/^(address1-)[a-zA-z0-9]+/));
-    await user.keyboard('432 PARK AVE');
-    await user.click(screen.getByTestId(/^(address2-)[a-zA-z0-9]+/));
-    await user.keyboard('Coral Springs, FL 07677');
-    await user.click(screen.getByTestId(/^(address3-)[a-zA-z0-9]+/));
-    await user.keyboard('United States');
-    await user.click(screen.getByTestId(/^(addressUrl-)[a-zA-z0-9]+/));
-    await user.keyboard('HTTP://WWW.WEBSITE.COM');
-    await user.click(screen.getByTestId(/^(emailOn-)[a-zA-z0-9]+/));
-    await user.click(screen.getByTestId(/^(email-)[a-zA-z0-9]+/));
-    await user.keyboard('contact');
-    await user.click(screen.getByTestId(/^(emailLink-)[a-zA-z0-9]+/));
-    await user.keyboard('contact@phillips.com');
-    await user.click(screen.getByTestId(/^(enableOnSite-)[a-zA-z0-9]+/).previousSibling as Element);
-
-    await user.click(screen.getByText(/SAVE DETAILS/));
-    expect(mockedHandleOnSave).toReturnWith(
-      expect.objectContaining({
-        address1: '432 PARK AVE',
-        address2: 'Coral Springs, FL 07677',
-        address3: 'United States',
-        addressUrl: 'HTTP://WWW.WEBSITE.COM',
-        enableOnSite: 'true',
-        location: 'One title',
-        previewDates: '3-6 November',
-        previewHours1: '8am - 9pm',
-        previewHours2: 'After closing',
-        previewLabelValue: 'Opening Night',
-        previewOn: 'true',
-        viewingDates: '3-6 December',
-        viewingHours1: '7am - 8pm',
-        viewingHours2: '9:30pm - 10pm',
-        viewingLabelValue: 'Open to the public',
-        emailOn: 'true',
-        email: 'contact',
-        emailLink: 'contact@phillips.com',
-      }),
-    );
-  });
+      await user.click(screen.getByText(/SAVE DETAILS/));
+      expect(mockedHandleOnSave).toReturnWith(
+        expect.objectContaining({
+          address1: '432 PARK AVE',
+          address2: 'Coral Springs, FL 07677',
+          address3: 'United States',
+          addressUrl: 'HTTP://WWW.WEBSITE.COM',
+          enableOnSite: 'true',
+          location: 'One title',
+          previewDates: '3-6 November',
+          previewHours1: '8am - 9pm',
+          previewHours2: 'After closing',
+          previewLabelValue: 'Opening Night',
+          previewOn: 'true',
+          viewingDates: '3-6 December',
+          viewingHours1: '7am - 8pm',
+          viewingHours2: '9:30pm - 10pm',
+          viewingLabelValue: 'Open to the public',
+          emailOn: 'true',
+          email: 'contact',
+          emailLink: 'contact@phillips.com',
+        }),
+      );
+    },
+    INCREASED_TEST_TIMEOUT,
+  );
 });
---
+++ src/utils/constants.ts
@@ -0,0 +1,2 @@
+export const INCREASED_TEST_TIMEOUT = 30 * 1000;
+// default test timeout is 5 seconds, that's not enough for certain tests, so we increase it to 30 seconds
