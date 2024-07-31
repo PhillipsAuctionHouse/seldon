77785132984cb948f9e4343e84e2909c55e907c4
Kostiantyn Huzenko <huzenko.c@gmail.com>
2024-07-31 19:16:19 -0400

    feat(footer): added onClick to social

--- src/components/Social/Social.tsx
+++ src/components/Social/Social.tsx
@@ -2,12 +2,21 @@ import classnames from 'classnames';
 
 import { getCommonProps } from '../../utils';
 import Button from '../Button/Button';
+import { MouseEventHandler } from 'react';
 
 export interface SocialProps extends React.HTMLAttributes<HTMLDivElement> {
   /**
    * Title text for the social section
    */
   titleText?: string;
+  /**
+   * Text for the subscribe button
+   */
+  buttonText?: string;
+  /**
+   * Function for when the subscribe button is clicked
+   */
+  onSubscribeClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
 /**
@@ -20,13 +29,20 @@ export interface SocialProps extends React.HTMLAttributes<HTMLDivElement> {
  * [Storybook Link](https://phillips-seldon.netlify.app/?path=/docs/components-social--overview)
  */
 
-const Social = ({ className, children, titleText = 'Never Miss A Moment', ...props }: SocialProps) => {
+const Social = ({
+  className,
+  children,
+  titleText = 'Never Miss A Moment',
+  buttonText = 'Subscribe To Our Newsletter',
+  onSubscribeClick,
+  ...props
+}: SocialProps) => {
   const { className: baseClassName, ...commonProps } = getCommonProps(props, 'Social');
   return (
     <div {...commonProps} className={classnames(baseClassName, className)} {...props}>
       <h3 className={`${baseClassName}__header`}>{titleText}</h3>
-      <Button buttonType="ghost" className={`${baseClassName}__button`}>
-        Subscribe To Our Newsletter
+      <Button onClick={onSubscribeClick} buttonType="ghost" className={`${baseClassName}__button`}>
+        {buttonText}
       </Button>
       {children}
     </div>
