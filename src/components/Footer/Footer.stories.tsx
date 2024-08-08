a90137bb0fe12ebab0e25ef15d2387043e2b0799
Kostiantyn Huzenko <huzenko.c@gmail.com>
2024-08-08 16:05:21 -0400

    feat(footer): L3-2980 footer mobile redesign

--- src/components/Footer/Footer.stories.tsx
+++ src/components/Footer/Footer.stories.tsx
@@ -9,7 +9,7 @@ import Wechat from '../../assets/wechat.svg?react';
 import Spotify from '../../assets/spotify.svg?react';
 import Social from '../Social/Social';
 import { Accordion, AccordionItem } from '../Accordion';
-import { useEffect, useState } from 'react';
+import { Text, TextVariants } from '../Text';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta = {
@@ -18,6 +18,63 @@ const meta = {
 } satisfies Meta<typeof Footer>;
 
 export default meta;
+
+const aboutUsLinks = (
+  <ul>
+    <li>
+      <a href="/">Our History</a>
+    </li>
+    <li>
+      <a href="/">Our Team</a>
+    </li>
+    <li>
+      <a href="/">Locations</a>
+    </li>
+    <li>
+      <a href="/">Press</a>
+    </li>
+    <li>
+      <a href="/">Careers</a>
+    </li>
+    <li>
+      <a href="/">Site map</a>
+    </li>
+  </ul>
+);
+
+const ourServicesLinks = (
+  <ul>
+    <li>
+      <a href="/">How to Buy</a>
+    </li>
+    <li>
+      <a href="/">How to Sell</a>
+    </li>
+    <li>
+      <a href="/">Private Services</a>
+    </li>
+    <li>
+      <a href="/">Trusts, Estates & Valuations</a>
+    </li>
+    <li>
+      <a href="/">Fiduciary Services</a>
+    </li>
+  </ul>
+);
+
+const policyLinks = (
+  <ul>
+    <li>
+      <a href="/">Privacy Policy</a>
+    </li>
+    <li>
+      <a href="/">Cookie Policy</a>
+    </li>
+    <li>
+      <a href="/">Modern Day Slavery Policy</a>
+    </li>
+  </ul>
+);
 
 const socialIcons = (
   <ul>
@@ -44,66 +101,44 @@ const socialIcons = (
   </ul>
 );
 
-const leftComponent = (viewPortWidth: number) => {
-  const isMobileViewPort = viewPortWidth >= 1301 ? false : true;
+const leftComponent = (
+  <>
+    <div className={`${px}-footer-desktop`}>
+      <div>
+        <Text variant={TextVariants.heading4}>About Us</Text>
+        {aboutUsLinks}
+      </div>
 
-  return (
-    <Accordion type="multiple" {...(isMobileViewPort ? {} : { value: ['About Us', 'Our Services', 'Policies'] })}>
+      <div>
+        <Text variant={TextVariants.heading4}>Our Services</Text>
+        {ourServicesLinks}
+      </div>
+
+      <div>
+        <Text variant={TextVariants.heading4}>Policies</Text>
+        {policyLinks}
+      </div>
+    </div>
+
+    <Accordion type="multiple" className={`${px}-footer-mobile`}>
       <AccordionItem
         isLocked={false}
         variation={''}
         label={'About Us'}
-        isControlled={isMobileViewPort}
         key={`accordion-key-about-us`}
         id={`accordion-item-about-us`}
       >
-        <ul>
-          <li>
-            <a href="/">Our History</a>
-          </li>
-          <li>
-            <a href="/">Our Team</a>
-          </li>
-          <li>
-            <a href="/">Locations</a>
-          </li>
-          <li>
-            <a href="/">Press</a>
-          </li>
-          <li>
-            <a href="/">Careers</a>
-          </li>
-          <li>
-            <a href="/">Site map</a>
-          </li>
-        </ul>
+        {aboutUsLinks}
       </AccordionItem>
 
       <AccordionItem
         isLocked={false}
         variation={''}
         label={'Our Services'}
-        isControlled={isMobileViewPort}
         key={`accordion-key-our-services`}
         id={`accordion-item-our-services`}
       >
-        <ul>
-          <li>
-            <a href="/">How to Buy</a>
-          </li>
-          <li>
-            <a href="/">How to Sell</a>
-          </li>
-          <li>
-            <a href="/">Private Services</a>
-          </li>
-          <li>
-            <a href="/">Trusts, Estates & Valuations</a>
-          </li>
-          <li>
-            <a href="/">Fiduciary Services</a>
-          </li>
-        </ul>
+        {ourServicesLinks}
       </AccordionItem>
 
       <AccordionItem
@@ -111,51 +146,23 @@ const leftComponent = (viewPortWidth: number) => {
         variation={''}
         label={'Policies'}
         isLastItem={true}
-        isControlled={isMobileViewPort}
         key={`accordion-key-policies`}
         id={`accordion-item-policies`}
       >
-        <ul>
-          <li>
-            <a href="/">Privacy Policy</a>
-          </li>
-          <li>
-            <a href="/">Cookie Policy</a>
-          </li>
-          <li>
-            <a href="/">Modern Day Slavery Policy</a>
-          </li>
-        </ul>
+        {policyLinks}
       </AccordionItem>
     </Accordion>
-  );
-};
+  </>
+);
 
 const rightComponent = <Social className={`${px}-footer__social`}>{socialIcons}</Social>;
 
-export const Playground = (props: FooterProps) => {
-  const [viewPortWidth, setViewPortWidth] = useState(
-    Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0),
-  );
-
-  useEffect(() => {
-    window.addEventListener('resize', () => {
-      const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-      setViewPortWidth(vw);
-    });
-
-    return () => {
-      window.removeEventListener('resize', () => ({}));
-    };
-  }, []);
-
-  return (
-    <Footer {...props}>
-      {leftComponent(viewPortWidth)}
-      {rightComponent}
-    </Footer>
-  );
-};
+export const Playground = (props: FooterProps) => (
+  <Footer {...props}>
+    {leftComponent}
+    {rightComponent}
+  </Footer>
+);
 
 Playground.args = {
   // navigationComponent,
