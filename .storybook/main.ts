49d919f8a6c68cb9643f7c2b71d10d0d68df57a9
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-08-22 08:55:17 -0500

    fix(fonts): fonts not loading in storybook preview

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -21,6 +21,7 @@ const config: StorybookConfig = {
       config.resolve.alias = {
         ...config.resolve.alias,
         '#scss': path.resolve(__dirname, '../src/scss/'),
+        '../fonts': path.resolve(__dirname, '../public/fonts/'),
       };
     }
     return {
