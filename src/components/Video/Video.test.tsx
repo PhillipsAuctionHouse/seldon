7dcceac43de28abd30e747f40271ec7fd608e003
Aaron Hill <aahill50@users.noreply.github.com>
2024-08-26 14:57:42 -0400

    feat(video): L3-2993 Create Video component (#283)
    * feat(video): create Video component
    
    * fix: minor updates
    
    * build: add figma link
    
    * chore: pr feedback
    
    * chore: pr feedback - remove height prop and set height to auto
    
    * chore: more pr feedback
    
    ---------
    
    Co-authored-by: Aaron Hill <AHill@phillips.com>

---
+++ src/components/Video/Video.test.tsx
@@ -0,0 +1,43 @@
+import { Video, type VideoProps } from './';
+import { runCommonTests } from '../../utils/testUtils';
+import { render, screen } from '@testing-library/react';
+
+describe('Video', () => {
+  runCommonTests(Video, 'Video');
+
+  const testProps = {
+    videoSource: 'https://www.example.com',
+  };
+
+  const renderVideo = (props: VideoProps) => {
+    render(<Video {...props} />);
+  };
+
+  it('should render a div with an iframe', () => {
+    renderVideo({ ...testProps });
+
+    const div = screen.getByTestId('video');
+    expect(div).toBeInTheDocument();
+
+    const iframe = div.querySelector('iframe');
+    expect(iframe).toBeInTheDocument();
+  });
+
+  it('should render an iframe with the correct src', () => {
+    renderVideo({ ...testProps });
+
+    const iframe = screen.getByTestId('video-iframe');
+    expect(iframe).toBeInTheDocument();
+    expect(iframe).toHaveAttribute('src', testProps.videoSource);
+  });
+
+  it('should render a div with the correct aspect ratio', () => {
+    const aspectRatio = 3 / 2;
+
+    renderVideo({ ...testProps, aspectRatio });
+
+    const div = screen.getByTestId('video');
+    expect(div).toBeInTheDocument();
+    expect(div.style.getPropertyValue('--aspect-ratio')).toBe(`${aspectRatio}`);
+  });
+});
