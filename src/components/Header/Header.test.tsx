98bec527581c78eb881fe4a33481d916e9349b5f
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-09-10 14:59:56 -0500

    fix(header): handle PR comments

--- src/components/Header/Header.test.tsx
+++ src/components/Header/Header.test.tsx
@@ -52,13 +52,6 @@ describe('Header', () => {
     await userEvent.click(toggleButton);
     expect(screen.getByRole('button', { name: /Close Menu/i })).toBeInTheDocument();
   });
-
-  // it('should expand a nav category in the mobile menu when a nav item trigger is selected', async () => {
-  //   render(headerComponent());
-  //   const navItemTrigger = screen.getByTestId(`navigation-item-trigger-auctions`);
-  //   await userEvent.click(navItemTrigger);
-  //   expect(navItemTrigger).toHaveClass(`${px}-nav__item--expanded`);
-  // });
 });
 
 describe('Header with logo', () => {
