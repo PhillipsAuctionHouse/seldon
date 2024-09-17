f0b120ae7e59b4fa8cf3b2a73f6976f7ebb5eaa0
PHILLIPS\ADietrich <adietrich@phillips.com>
2024-09-17 11:51:15 -0400

    fix(header-submenus): L3-2818 removed unused overlay component

--- src/components/Overlay/Overlay.test.tsx
+++
@@ -1,6 +0,0 @@
-import Overlay from './Overlay';
-import { runCommonTests } from '../../utils/testUtils';
-
-describe('Overlay', () => {
-  runCommonTests(Overlay, 'Overlay');
-});
--- src/components/Overlay/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as Overlay, type OverlayProps } from './Overlay';
