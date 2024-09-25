fd2bcb1cb53ea6833a9c965f4d5885de3ec4161a
Kostiantyn Huzenko <huzenko.c@gmail.com>
2024-09-25 13:15:54 -0400

    chore: screenshot testing

--- src/site-furniture/Footer/Footer.test.tsx
+++ src/site-furniture/Footer/Footer.test.tsx
@@ -1,89 +1,19 @@
-import { render, screen } from '@testing-library/react';
 // import { userEvent } from '@testing-library/user-event';
 import { toMatchImageSnapshot } from 'jest-image-snapshot';
-
-import Footer from './Footer';
-import Social from '../../patterns/Social/Social';
-import Subscribe from '../../patterns/Subscribe/Subscribe';
-import puppeteer from 'puppeteer';
 expect.extend({ toMatchImageSnapshot });
 
 describe('Footer', () => {
-  const socialIcons = (
-    <ul>
-      <li>
-        <a>Youtube</a>
-      </li>
-      <li>
-        <a>Instagram</a>
-      </li>
-      <li>
-        <a>Wechat</a>
-      </li>
-      <li>
-        <a>Spotify</a>
-      </li>
-    </ul>
-  );
-
-  const leftComponent = (
-    <Subscribe
-      id="subscribe"
-      title="Subscribe to Email"
-      blurb="This blurb will be rendered"
-      buttonProps={{ onClick: vi.fn() }}
-    />
-  );
-
-  const rightComponent = <Social id="social">{socialIcons}</Social>;
-  const child = (
-    <>
-      {leftComponent}
-      {rightComponent}
-    </>
-  );
-
-  const navigationComponent = (
-    <ul>
-      <li>
-        <a>Locations</a>
-      </li>
-      <li>
-        <a>Press</a>
-      </li>
-      <li>
-        <a>Careers</a>
-      </li>
-      <li>
-        <a>Privacy policy</a>
-      </li>
-      <li>
-        <a>Cookie policy</a>
-      </li>
-      <li>
-        <a>Modern Slavery Policy</a>
-      </li>
-    </ul>
-  );
-
-  const commonProps = {
-    children: child,
-    navigationComponent,
-  };
-
   // it('is selectable by the test id', () => {
   //   const { rerender } = render(<Footer {...commonProps} />);
   //   expect(screen.queryByTestId('footer')).toBeInTheDocument();
   //   rerender(<Footer id="test" {...commonProps} />);
   //   expect(screen.queryByTestId('test')).toBeInTheDocument();
   // });
-
   // it('renders the components we pass to it', () => {
   //   render(<Footer {...commonProps} />);
   //   expect(screen.queryByTestId(/subscribe/)).toBeInTheDocument();
   //   expect(screen.queryByTestId(/social/)).toBeInTheDocument();
   // });
-
   // it('screenshot', async () => {
   //   const browser = await puppeteer.launch({
   //     headless: false,
@@ -96,7 +26,6 @@ describe('Footer', () => {
   //     type: 'png',
   //     fullPage: true,
   //   });
-
   //   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   //   // @ts-ignore
   //   expect(image).toMatchImageSnapshot({
