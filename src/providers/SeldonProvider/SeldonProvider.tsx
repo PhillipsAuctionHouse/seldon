fcb6fcf9b5e485c24c3ad15c811b83a14427bc62
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-10-04 09:36:31 -0500

    docs(seldonprovider): mention the reset styles

--- src/providers/SeldonProvider/SeldonProvider.tsx
+++ src/providers/SeldonProvider/SeldonProvider.tsx
@@ -3,7 +3,9 @@ import { SSRMediaQuery, ssrMediaQueryStyle } from '../../providers/utils';
 import './_seldonProvider.scss';
 import { px } from '../../utils';
 /**
- * The SeldonProvider currently provides the ability for our components to render differently based on different media queries, but in the future it may provide other kinds of context data like common internationalization (i18n) data.
+ * The SeldonProvider provides the ability for our components to render differently based on different media queries,
+ * It also applies a global reset to the browser's default styles for all seldon components within.
+ * In the future it may provide other kinds of context data like common internationalization (i18n) data.
  *
  * It is recommended to wrap this provider once around your entire application to ensure that all Seldon components can access the same context.
  */
