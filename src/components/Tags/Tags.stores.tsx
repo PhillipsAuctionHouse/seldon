210f6c54bed7fe1f5820bd7df37a70c39c678819
Zhenting <zhentzhou@gmail.com>
2024-10-22 12:55:46 -0400

    test storybook

--- src/components/Tags/Tags.stores.tsx
+++ src/components/Tags/Tags.stores.tsx
@@ -1,2 +1,50 @@
 import type { Meta } from '@storybook/react';
 import { useState } from 'react';
+
+import TagsList, { TagsListProps } from './Tags';
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+    title: 'Components/Tags',
+    component: TagsList,
+} satisfies Meta<typeof TagsList>;
+
+export default meta;
+
+interface StoryProps extends TagsListProps {
+    playgroundWidth: string;
+}
+
+const tagsListExample = ["Jean-Michel Basquiat", "Cecily Brown", "Roy Lichtenstein", "Amy Sherald", "Cy Twombly", "Andy Warhol"]
+
+const argTypes = {
+    onClear: {
+        action: 'onClear',
+    },
+    playgroundWidth: {
+        control: { type: 'range', min: 200, max: 300 },
+    },
+};
+export const Playground = ({ playgroundWidth, ...args }: StoryProps) => {
+    // Parent component is in charge of the state management
+    const [tagsList, setTagsList] = useState(tagsListExample);
+
+    return (
+        <div style={{ width: playgroundWidth, margin: '1rem' }}>
+            <TagsList
+                {...args}
+                id="TagsList-1"
+                tagsList={tagsList}
+                onClear={() => {
+                    setTagsList([])
+                }}
+            ></TagsList>
+        </div>
+    );
+};
+
+Playground.args = {
+    playgroundWidth: 200,
+    className: 'tags-test-class',
+};
+
+Playground.argTypes = argTypes;
--- src/components/Tags/Tags.tsx
+++ src/components/Tags/Tags.tsx
@@ -1,11 +1,91 @@
 import classnames from 'classnames';
-
 import { getCommonProps } from '../../utils';
+import { px } from '../../utils';
+import Button from '../Button/Button';
+import IconButton from '../IconButton/IconButton';
+import ChevronRight from '../../assets/chevronRight.svg?react';
 
-const Tags = ({ }) => {
-    return <div>
+export interface TagsListProps {
+    /**
+    * Unique id for component testing
+    */
+    id: string;
+    /**
+    * Base class for TagsList component.
+    */
+    className?: string;
+    /**
+    * List of tags to render
+    */
+    tagsList: string[]
+    /**
+    * `onRemove` callback removes the clicked tag from the list
+    */
+    onRemove: (tag: string) => void;
+    /**
+    * `onClear` callback removes all the string and set the list to empty
+    */
+    onClear: () => void;
+}
 
+export interface TagsProps {
+    /**
+    * Base class for TagsList component.
+    */
+    className?: string;
+    /**
+    * `onRemove` callback removes the clicked tag from the list
+    */
+    onRemove: (tag: string) => void;
+    /**
+    * tag is a string render within tag component
+    */
+    tag: string;
+}
+
+const Tags = ({
+    className,
+    onRemove,
+    tag,
+}: TagsProps) => {
+    return <Button className={className}>
+        <>
+            <div>{tag}</div>
+            <div onClick={() => onRemove(tag)}>
+                <IconButton className={``}>
+                    <span>X</span>
+                </IconButton>
+            </div>
+        </>
+    </Button>
+}
+
+const TagsList = ({
+    className,
+    onRemove,
+    onClear,
+    tagsList = [],
+    ...props
+}: TagsListProps) => {
+    const type = 'tagsList';
+    const { className: baseClassName, ...commonProps } = getCommonProps(props, 'TagsList');
+    const { id } = props;
+
+    return <div className={classnames(`${px}-${type}`, { [`${baseClassName}__wrapper`]: baseClassName }, className)}
+        {...commonProps}
+        {...props}
+        data-testid={`${id}-tagsList`}
+    >
+        {tagsList.map((tag) => {
+            return <Tags tag={tag} onRemove={onRemove} />
+        })}
+        <Button onClick={onClear} data-testid={`${id}-onClear-button`}>
+            <IconButton className={`${px}-left-arrow`}>
+                <ChevronRight />
+            </IconButton>
+            Clear All
+        </Button>
     </div>
 }
 
-export default Tags;
+export default TagsList;
