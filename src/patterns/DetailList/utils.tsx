3e20bf07a0f1915a156e73f4b5a858f0b5ced664
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-11-13 10:23:34 -0600

    fix(input): L3-4544 input style font size fix and DetailList key fix (#422)

--- src/patterns/DetailList/utils.tsx
+++ src/patterns/DetailList/utils.tsx
@@ -3,6 +3,9 @@ import { DetailComponent, DetailProps } from '../../components/Detail';
 
 export const getDetailKey = (child: DetailComponent, index: number) => {
   if (isValidElement(child)) {
+    if (child.key) {
+      return child.key;
+    }
     const { label, value } = child.props as DetailProps;
     return `detail-${label}-${value}`;
   }
