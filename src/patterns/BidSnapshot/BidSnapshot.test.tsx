d1bd3268574db6a8105b5f93a5572614be1bf6f5
Kostiantyn Huzenko <huzenko.c@gmail.com>
2024-12-02 14:06:51 -0500

    feat(BidSnapshot): L3-4690 changed prop (#438)
    Co-authored-by: Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>

--- src/patterns/BidSnapshot/BidSnapshot.test.tsx
+++ src/patterns/BidSnapshot/BidSnapshot.test.tsx
@@ -1,20 +1,20 @@
 import BidSnapshot from './BidSnapshot';
 import { runCommonTests } from '../../utils/testUtils';
 import { render, screen } from '@testing-library/react';
-import { AuctionStatus } from '../../types/commonTypes';
+import { LotStatus } from '../../types/commonTypes';
 import { BidStatusEnum } from './types';
 
 describe('BidSnapshot', () => {
   runCommonTests(BidSnapshot, 'BidSnapshot');
 
   it('renders starting bid when there is only one bid and auction is not past', () => {
-    render(<BidSnapshot startingBid={100} auctionStatus={AuctionStatus.ready} />);
+    render(<BidSnapshot startingBid={100} lotStatus={LotStatus.ready} />);
     expect(screen.getByText('Starting bid')).toBeInTheDocument();
     expect(screen.getByText('$100')).toBeInTheDocument();
   });
 
   it('renders current bid when there are multiple bids and auction is not past', () => {
-    render(<BidSnapshot startingBid={100} numberOfBids={3} auctionStatus={AuctionStatus.live} currentBid={300} />);
+    render(<BidSnapshot startingBid={100} numberOfBids={3} lotStatus={LotStatus.live} currentBid={300} />);
     expect(screen.getByText('Current bid')).toBeInTheDocument();
     expect(screen.getByText('$300')).toBeInTheDocument();
     expect(screen.getByText('(3 bids)')).toBeInTheDocument();
@@ -26,7 +26,7 @@ describe('BidSnapshot', () => {
         startingBid={100}
         currentBid={300}
         numberOfBids={3}
-        auctionStatus={AuctionStatus.past}
+        lotStatus={LotStatus.past}
         bidStatus={BidStatusEnum.Won}
       />,
     );
@@ -40,7 +40,7 @@ describe('BidSnapshot', () => {
         startingBid={100}
         numberOfBids={3}
         currentBid={300}
-        auctionStatus={AuctionStatus.past}
+        lotStatus={LotStatus.past}
         bidStatus={BidStatusEnum.Lost}
       />,
     );
@@ -50,9 +50,7 @@ describe('BidSnapshot', () => {
 
   it('renders Countdown timer when lotCloseDate prop is passed', () => {
     const lotCloseDate = new Date(Date.now() + 10000); // 10 seconds from now
-    render(
-      <BidSnapshot startingBid={100} numberOfBids={1} auctionStatus={AuctionStatus.live} lotCloseDate={lotCloseDate} />,
-    );
+    render(<BidSnapshot startingBid={100} numberOfBids={1} lotStatus={LotStatus.live} lotCloseDate={lotCloseDate} />);
     expect(screen.getByText('Closes in')).toBeInTheDocument();
   });
 });
--- src/patterns/BidSnapshot/BidSnapshot.tsx
+++ src/patterns/BidSnapshot/BidSnapshot.tsx
@@ -4,7 +4,7 @@ import classnames from 'classnames';
 import { findChildrenExcludingTypes, findChildrenOfType, getCommonProps } from '../../utils';
 import { DetailList } from '../DetailList/index';
 import { Detail } from '../../components/Detail/index';
-import { AuctionStatus, SupportedLanguages } from '../../types/commonTypes';
+import { LotStatus, SupportedLanguages } from '../../types/commonTypes';
 import { Countdown } from '../../components/Countdown/index';
 import { CountdownVariants } from '../../components/Countdown/types';
 import { BidStatusEnum } from './types';
@@ -18,7 +18,7 @@ export interface BidSnapshotProps extends ComponentProps<'div'> {
   /**
    * State of the object
    */
-  auctionStatus?: AuctionStatus;
+  lotStatus?: LotStatus;
   /**
    * Bids label text, a fucntion for label of bids amoutn (2 bids, 3 bids, etc) where the number is the length of the bids array.
    */
@@ -86,8 +86,8 @@ const bidsTranslation = (numberOfBids: number) => (numberOfBids === 1 ? `${numbe
 const BidSnapshot = forwardRef<HTMLDivElement, BidSnapshotProps>(
   (
     {
+      lotStatus = LotStatus.ready,
       bidStatus,
-      auctionStatus = AuctionStatus.ready,
       bidsLabelText = bidsTranslation,
       children,
       className,
@@ -111,9 +111,9 @@ const BidSnapshot = forwardRef<HTMLDivElement, BidSnapshotProps>(
 
     const hasBids = currentBid !== null && numberOfBids > 0;
     const isTopBid = bidStatus === BidStatusEnum.Winning || bidStatus === BidStatusEnum.Won;
-    const isReady = auctionStatus === AuctionStatus.ready;
-    const isLive = auctionStatus === AuctionStatus.live;
-    const isPast = auctionStatus === AuctionStatus.past;
+    const isReady = lotStatus === LotStatus.ready;
+    const isLive = lotStatus === LotStatus.live;
+    const isPast = lotStatus === LotStatus.past;
     const hasCountdownTimer = isLive && lotCloseDate;
     const bidMessage = findChildrenOfType(children, BidMessage);
     const otherChildren = findChildrenExcludingTypes(children, [BidMessage]);
