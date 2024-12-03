a6145dc2b3218a36f3d87e7152f5420df02d1d95
Scott Dickerson <6663002+scottdickerson@users.noreply.github.com>
2024-12-03 08:49:01 -0600

    fix(bidsnapshot): L3-4378 if i have bid on a lot but lost it should say won for (#443)

--- src/patterns/BidSnapshot/BidSnapshot.test.tsx
+++ src/patterns/BidSnapshot/BidSnapshot.test.tsx
@@ -3,6 +3,7 @@ import { runCommonTests } from '../../utils/testUtils';
 import { render, screen } from '@testing-library/react';
 import { LotStatus } from '../../types/commonTypes';
 import { BidStatusEnum } from './types';
+import BidMessage from './BidMessage';
 
 describe('BidSnapshot', () => {
   runCommonTests(BidSnapshot, 'BidSnapshot');
@@ -11,6 +12,18 @@ describe('BidSnapshot', () => {
     render(<BidSnapshot startingBid={100} lotStatus={LotStatus.ready} />);
     expect(screen.getByText('Starting bid')).toBeInTheDocument();
     expect(screen.getByText('$100')).toBeInTheDocument();
+  });
+
+  it('renders starting bid if no bids placed', () => {
+    render(<BidSnapshot startingBid={100} lotStatus={LotStatus.live} />);
+    expect(screen.getByText('Starting bid')).toBeInTheDocument();
+    expect(screen.getByText('$100')).toBeInTheDocument();
+  });
+
+  it('does not render starting bid if lot did not sell', () => {
+    render(<BidSnapshot startingBid={100} lotStatus={LotStatus.past} />);
+    expect(screen.queryByText('Starting bid')).not.toBeInTheDocument();
+    expect(screen.queryByText('$100')).not.toBeInTheDocument();
   });
 
   it('renders current bid when there are multiple bids and auction is not past', () => {
@@ -24,33 +37,97 @@ describe('BidSnapshot', () => {
     render(
       <BidSnapshot
         startingBid={100}
-        currentBid={300}
+        soldPrice={300}
         numberOfBids={3}
         lotStatus={LotStatus.past}
         bidStatus={BidStatusEnum.Won}
-      />,
+      >
+        <BidMessage message="You won" />
+      </BidSnapshot>,
     );
     expect(screen.getByText('Won for')).toBeInTheDocument();
     expect(screen.getByText('$300')).toBeInTheDocument();
+    expect(screen.getByText('You won')).toBeInTheDocument();
   });
 
-  it('renders sold for when auction is past and active bid is not the last bid', () => {
+  it('does not render bid message if user has not bid', () => {
+    render(
+      <BidSnapshot startingBid={100} soldPrice={300} numberOfBids={3} lotStatus={LotStatus.past}>
+        <BidMessage message="You won" />
+      </BidSnapshot>,
+    );
+    expect(screen.queryByText('Won for')).not.toBeInTheDocument();
+    expect(screen.getByText('Sold for')).toBeInTheDocument();
+    expect(screen.getByText('$300')).toBeInTheDocument();
+    expect(screen.queryByText('You won')).not.toBeInTheDocument();
+  });
+
+  it('renders won for instead of sold for when auction is past user lost', () => {
     render(
       <BidSnapshot
         startingBid={100}
         numberOfBids={3}
         currentBid={300}
+        soldPrice={300}
         lotStatus={LotStatus.past}
         bidStatus={BidStatusEnum.Lost}
-      />,
+      >
+        <BidMessage message="You lost"></BidMessage>
+      </BidSnapshot>,
     );
-    expect(screen.getByText('Sold for')).toBeInTheDocument();
+    expect(screen.getByText('Won for')).toBeInTheDocument();
     expect(screen.getByText('$300')).toBeInTheDocument();
+    expect(screen.getByText('You lost')).toBeInTheDocument();
+  });
+
+  it('renders winning for if in progress', () => {
+    render(
+      <BidSnapshot
+        startingBid={100}
+        numberOfBids={3}
+        currentBid={300}
+        lotStatus={LotStatus.live}
+        bidStatus={BidStatusEnum.Lost}
+      >
+        <BidMessage message="Winning for"></BidMessage>
+      </BidSnapshot>,
+    );
+    expect(screen.getByText('Winning for')).toBeInTheDocument();
+    expect(screen.getByText('$300')).toBeInTheDocument();
+  });
+
+  it('does not render sold for if soldPrice not passed, used if auction should hide sold price', () => {
+    render(
+      <BidSnapshot
+        startingBid={100}
+        currentBid={500}
+        numberOfBids={3}
+        lotStatus={LotStatus.past}
+        bidStatus={BidStatusEnum.Lost}
+      >
+        <BidMessage message="You won" />
+      </BidSnapshot>,
+    );
+    expect(screen.queryByText('Sold for')).not.toBeInTheDocument();
+    expect(screen.queryByText('$500')).not.toBeInTheDocument();
+    expect(screen.getByText('You won')).toBeInTheDocument();
   });
 
   it('renders Countdown timer when lotCloseDate prop is passed', () => {
     const lotCloseDate = new Date(Date.now() + 10000); // 10 seconds from now
     render(<BidSnapshot startingBid={100} numberOfBids={1} lotStatus={LotStatus.live} lotCloseDate={lotCloseDate} />);
     expect(screen.getByText('Closes in')).toBeInTheDocument();
+  });
+
+  it('does not render Countdown timer if lot closed', () => {
+    const lotCloseDate = new Date(Date.now() + 10000); // 10 seconds from now
+    render(<BidSnapshot startingBid={100} numberOfBids={1} lotStatus={LotStatus.past} lotCloseDate={lotCloseDate} />);
+    expect(screen.queryByText('Closes in')).not.toBeInTheDocument();
+  });
+
+  it('does not render Countdown timer if lot not started', () => {
+    const lotCloseDate = new Date(Date.now() + 10000); // 10 seconds from now
+    render(<BidSnapshot startingBid={100} numberOfBids={1} lotStatus={LotStatus.ready} lotCloseDate={lotCloseDate} />);
+    expect(screen.queryByText('Closes in')).not.toBeInTheDocument();
   });
 });
