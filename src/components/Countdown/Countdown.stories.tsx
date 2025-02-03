d0a64ed9eb1739ae2ab09554eedd2481bb5c4e95
Aaron Hill <AHill@phillips.com>
2025-02-03 16:46:32 -0500

    fix(Countdown): L3-5249 do not display countdown timer after timer expires

--- src/components/Countdown/Countdown.stories.tsx
+++ src/components/Countdown/Countdown.stories.tsx
@@ -1,6 +1,6 @@
 import { Meta } from '@storybook/react';
 import Countdown, { CountdownProps } from './Countdown';
-import { addDays, addHours, addMinutes } from 'date-fns';
+import { addDays, addHours, addMinutes, addSeconds } from 'date-fns';
 import { CountdownVariants } from './types';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
@@ -14,6 +14,7 @@ export default meta;
 const daysDate = addDays(new Date(), 3);
 const hoursDate = addHours(new Date(), 5);
 const minutesDate = addMinutes(new Date(), 20);
+const secondsDate = addSeconds(new Date(), 10);
 
 export const Playground = (props: CountdownProps) => <Countdown {...props} endDateTime={minutesDate} />;
 
@@ -29,6 +30,8 @@ export const Days = (props: CountdownProps) => <Countdown {...props} endDateTime
 export const Hours = (props: CountdownProps) => <Countdown {...props} endDateTime={hoursDate} />;
 
 export const Minutes = (props: CountdownProps) => <Countdown {...props} endDateTime={minutesDate} />;
+
+export const Seconds = (props: CountdownProps) => <Countdown {...props} endDateTime={secondsDate} />;
 
 export const Compact = (props: CountdownProps) => (
   <Countdown {...props} endDateTime={minutesDate} variant={CountdownVariants.compact} />
