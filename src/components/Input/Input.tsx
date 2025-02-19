b8068979f40c4c780dbad725855908a15a256b3f
Sam Grund <sam@ip-192-168-1-164.ec2.internal>
2025-02-19 11:34:05 -0500

    feat(input): L3-5461 react node as input label

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -49,7 +49,7 @@ export interface InputProps extends Omit<React.ComponentProps<'input'>, 'size'> 
   /**
    * Text that will be read by a screen reader when visiting this control
    */
-  labelText: string;
+  labelText: React.ReactNode;
 
   /**
    * Optional `onChange` handler that is called whenever `<input>` is updated
