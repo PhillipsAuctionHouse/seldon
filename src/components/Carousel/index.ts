f1fb62fa3f3f522faba15ac9ef6c7a9d955018d8
Alexander Dietrich <46032334+adietrich3074@users.noreply.github.com>
2025-02-20 08:48:40 -0500

    feat(carousel-arrows): L3-5188 Carousel Navigation UI Fixes (#518)

--- src/components/Carousel/index.ts
+++ src/components/Carousel/index.ts
@@ -1,4 +1,5 @@
 export { default as Carousel, type CarouselProps, type CarouselApi } from './Carousel';
+export { default as CarouselArrows, type CarouselArrowsProps } from './CarouselArrows';
 export { default as CarouselContent, type CarouselContentProps } from './CarouselContent';
 export { default as CarouselItem, type CarouselItemProps } from './CarouselItem';
 export { default as CarouselDots, type CarouselDotsProps } from './CarouselDots';
