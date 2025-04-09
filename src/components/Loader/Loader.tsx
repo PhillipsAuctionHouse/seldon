e0639ccc2a74274413f656d91c5bdbc6ccb61c43
Zhenting <zhentzhou@gmail.com>
2025-04-09 15:09:21 -0400

    accessibility update

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -2,9 +2,19 @@ import { forwardRef, type HTMLProps } from 'react';
 import { px } from '../../utils';
 import './loader.scss';
 
-const Loader = forwardRef<HTMLSpanElement, HTMLProps<HTMLSpanElement>>((props, ref) => (
-  <span ref={ref} {...props} className={`${px}-loader`} data-testid="loader"></span>
-));
+const Loader = forwardRef<HTMLSpanElement, HTMLProps<HTMLSpanElement>>(
+  ({ 'aria-label': ariaLabel = 'Loading', ...props }, ref) => (
+    <span
+      ref={ref}
+      {...props}
+      className={`${px}-loader`}
+      data-testid="loader"
+      role="status"
+      aria-live="polite"
+      aria-label={ariaLabel}
+    ></span>
+  ),
+);
 
 Loader.displayName = 'Loader';
 export default Loader;
