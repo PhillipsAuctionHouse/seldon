67aa7eaf250e322861f34cfe9cfe76019865799e
Zhenting <zhentzhou@gmail.com>
2025-04-23 14:37:51 -0400

    fix(Loader): comment fix

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,13 +1,20 @@
-import { forwardRef, type HTMLProps } from 'react';
+import { forwardRef } from 'react';
 import { px } from '../../utils';
 import './loader.scss';
 
-const Loader = forwardRef<HTMLSpanElement, HTMLProps<HTMLSpanElement>>(
-  ({ 'aria-label': ariaLabel = 'Loading', ...props }, ref) => (
+export interface LoaderProps {
+  /** Props for the Loader component */
+  isCentered?: boolean;
+  /** Accessible label for the loader */
+  'aria-label'?: string;
+}
+
+const Loader = forwardRef<HTMLSpanElement, LoaderProps>(
+  ({ 'aria-label': ariaLabel = 'Loading', isCentered = true, ...props }, ref) => (
     <span
       ref={ref}
       {...props}
-      className={`${px}-loader`}
+      className={`${px}-loader${isCentered ? ` ${px}-loader--centered` : ''}`}
       data-testid="loader"
       role="status"
       aria-live="polite"
