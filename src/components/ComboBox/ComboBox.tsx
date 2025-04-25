46892e8dbf48c7cf9c95a905016c760b78143780
Zhenting <zhentzhou@gmail.com>
2025-04-25 10:16:22 -0400

    feat: A ComboBox feature

--- src/components/ComboBox/ComboBox.tsx
+++ src/components/ComboBox/ComboBox.tsx
@@ -38,16 +38,6 @@ export interface ComboBoxProps {
   setInputValue: (value: string) => void;
 }
 
-/**
- * ## Overview
- *
- * Overview of this widget
- *
- * [Figma Link](https://www.figma.com/design/hMu9IWH5N3KamJy8tLFdyV/EASEL-Compendium%3A-Tokens-%2C-Components-%26-Patterns?node-id=11973-9589&m=dev)
- *
- * [Storybook Link](https://phillips-seldon.netlify.app/?path=/docs/components-ComboBox--overview)
- */
-
 const ComboBox = React.forwardRef<HTMLDivElement, ComboBoxProps>(function ComboBox(
   { options = [], className, id, label, placeholder, inputValue = '', setInputValue, ...props },
   ref,
@@ -107,7 +97,7 @@ const ComboBox = React.forwardRef<HTMLDivElement, ComboBoxProps>(function ComboB
           }, 0);
         }}
       >
-        <Popover.Root open>
+        <Popover.Root open={true}>
           <Popover.Trigger asChild>
             <CommandInput
               placeholder={placeholder}
