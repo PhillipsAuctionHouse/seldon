817f1ca1cde7b66f1c46d4420000180cec36551f
Dave McNamara <dmcnamara@phillips.com>
2025-05-28 16:51:56 -0400

    feat(textbox): L3-6790 Implement invalid/warn/disabled states and messaging in TextArea component (#625)

--- src/components/TextArea/TextArea.tsx
+++ src/components/TextArea/TextArea.tsx
@@ -1,7 +1,7 @@
 import { ComponentProps, forwardRef, useId } from 'react';
 import classnames from 'classnames';
 
-import { px } from '../../utils';
+import { px, useNormalizedInputProps } from '../../utils';
 import { Icon } from '../Icon';
 
 export interface TextAreaProps extends ComponentProps<'textarea'> {
@@ -17,6 +17,28 @@ export interface TextAreaProps extends ComponentProps<'textarea'> {
    * Text that will be read by a screen reader when visiting this control
    */
   labelText: React.ReactNode;
+
+  /**
+   * Boolean to specify whether the control is disabled
+   */
+  disabled?: boolean;
+  /**
+   * Boolean to specify whether the control is currently in an invalid state
+   */
+  invalid?: boolean;
+  /**
+   * Text that is displayed when the control is in an invalid state
+   */
+  invalidText?: React.ReactNode;
+
+  /**
+   * Boolean to specify whether the control is currently in warning state
+   */
+  warn?: boolean;
+  /**
+   * Text that is displayed when the control is in warning state
+   */
+  warnText?: React.ReactNode;
 }
 
 /**
@@ -29,40 +51,79 @@ export interface TextAreaProps extends ComponentProps<'textarea'> {
  * [Storybook Link](https://phillips-seldon.netlify.app/?path=/docs/components-textarea--overview)
  */
 const TextArea = forwardRef<HTMLTextAreaElement, TextAreaProps>(
-  ({ className, id, isSkeletonLoading, labelText, maxLength = 3000, name, rows = 2, ...props }, ref) => {
+  (
+    {
+      className,
+      id,
+      isSkeletonLoading,
+      labelText,
+      maxLength = 3000,
+      name,
+      rows = 2,
+      disabled,
+      warn,
+      warnText,
+      invalid,
+      invalidText,
+      ...props
+    },
+    ref,
+  ) => {
     const baseClassName = `${px}-text-area`;
     const generatedId = useId();
+
+    const textAreaProps = useNormalizedInputProps({
+      disabled,
+      id: id ?? generatedId,
+      invalid,
+      invalidText,
+      type: 'text-area',
+      warn,
+      warnText,
+    });
+
+    const wrapperClassNames = classnames(`${baseClassName}__wrapper`, {
+      [`${px}-text-area--disabled`]: textAreaProps.disabled,
+      [`${px}-text-area--invalid`]: textAreaProps.invalid,
+      [`${px}-text-area--warn`]: textAreaProps.warn,
+    });
+
     return (
-      <div className={classnames(`${baseClassName}__wrapper`)}>
-        <label
-          data-testid={`text-area-${id ?? generatedId}-label`}
-          htmlFor={id ?? generatedId}
-          className={classnames(`${px}-input__label`, {
-            [`${px}-skeleton`]: isSkeletonLoading,
-          })}
-        >
-          {labelText}
-        </label>
-        <textarea
-          {...props}
-          className={classnames(baseClassName, className, {
-            [`${px}-skeleton`]: isSkeletonLoading,
-          })}
-          id={id ?? generatedId}
-          rows={rows}
-          maxLength={maxLength}
-          name={name}
-          ref={ref}
-          data-testid={`text-area-${id ?? generatedId}-input`}
-        />
-        <div className={`${baseClassName}-resizer__icon`}>
-          <Icon icon="Menu" />
+      <>
+        <div className={wrapperClassNames}>
+          <label
+            data-testid={`text-area-${id ?? generatedId}-label`}
+            htmlFor={id ?? generatedId}
+            className={classnames(`${px}-text-area__label`, {
+              [`${px}-skeleton`]: isSkeletonLoading,
+            })}
+          >
+            {labelText}
+          </label>
+
+          <textarea
+            {...props}
+            className={classnames(baseClassName, className, `${px}-text-area__input`, {
+              [`${px}-skeleton`]: isSkeletonLoading,
+            })}
+            id={id ?? generatedId}
+            rows={rows}
+            maxLength={maxLength}
+            name={name}
+            ref={ref}
+            data-testid={`text-area-${id ?? generatedId}-input`}
+            disabled={textAreaProps.disabled}
+          />
+
+          <Icon icon="Menu" className={`${baseClassName}-resizer__icon`} />
         </div>
-      </div>
+        <div>
+          {textAreaProps.validation ?? <div className={`${px}-text-area__validation`} id={textAreaProps.invalidId} />}
+        </div>
+      </>
     );
   },
 );
 
 TextArea.displayName = 'TextArea';
-
 export default TextArea;
