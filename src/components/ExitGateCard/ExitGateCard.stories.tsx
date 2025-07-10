0d6eee0d3e1585dcd27d3e1f099ff45305122573
Zhenting <zhenpanda@users.noreply.github.com>
2025-07-10 15:12:12 -0400

    feat(Article): L3-6952 Create exit gate card (#639)

---
+++ src/components/ExitGateCard/ExitGateCard.stories.tsx
@@ -0,0 +1,26 @@
+import { Meta } from '@storybook/react';
+import ExitGateCard, { ExitGateCardProps } from './ExitGateCard';
+
+// More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
+const meta = {
+  title: 'Components/ExitGateCard',
+  component: ExitGateCard,
+} satisfies Meta<typeof ExitGateCard>;
+
+export default meta;
+export const Playground = (props: ExitGateCardProps) => (
+  <div style={{ maxWidth: '1392px' }}>
+    <ExitGateCard {...props} />
+  </div>
+);
+
+// More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
+Playground.args = {
+  imageSrc: 'https://picsum.photos/704/268',
+  label: 'CONSIGN TODAY',
+  header: 'Sell with us',
+  description: 'We are inviting consignments for our upcoming auctions.',
+  linkLabel: 'Enquire now',
+  linkHref: '/',
+  variant: 'secondary',
+};
---
+++ src/components/ExitGateCard/ExitGateCard.tsx
@@ -0,0 +1,121 @@
+import classnames from 'classnames';
+import { ComponentProps, ElementType, forwardRef, ReactNode } from 'react';
+import { getCommonProps } from '../../utils';
+import Button from '../Button/Button';
+import { ButtonVariants } from '../Button/types';
+import { Link } from '../Link';
+import { SeldonImage } from '../SeldonImage';
+import { Text, TextVariants } from '../Text';
+
+export interface ExitGateCardProps extends ComponentProps<'div'> {
+  /**
+   * Image src to display at the right side of the article.
+   */
+  imageSrc?: string;
+  /**
+   * Top label for the article.
+   */
+  label?: ReactNode;
+  /**
+   * Header for the article.
+   */
+  header?: ReactNode;
+  /**
+   * Description for the article.
+   */
+  description?: ReactNode;
+  /**
+   * Custom link element to use for the link. Defaults to the `Link` component.
+   */
+  linkElement?: ElementType<ComponentProps<'a'>>;
+  /**
+   * Label for the link.
+   */
+  linkLabel?: ReactNode;
+  /**
+   * Href for the link.
+   */
+  linkHref?: string;
+  /**
+   * imageSrc alt text for accessibility.
+   */
+  altText?: string;
+  /**
+   * LinkVariants for the ExitGateCard.
+   */
+  variant?: ButtonVariants;
+}
+/**
+ * ## Overview
+ *
+ * Overview of this widget
+ *
+ * [Figma Link](https://www.figma.com/design/hMu9IWH5N3KamJy8tLFdyV/Design-System--Foundations--RW-to-be-depreciated-?node-id=24825-524&p=f&m=dev)
+ *
+ * [Storybook Link](https://phillips-seldon.netlify.app/?path=/docs/components-ExitGateCard-overview)
+ */
+const ExitGateCard = forwardRef<HTMLDivElement, ExitGateCardProps>(
+  (
+    {
+      className,
+      imageSrc,
+      label,
+      header,
+      description,
+      linkLabel,
+      linkHref,
+      linkElement: Component = Link,
+      variant = ButtonVariants.secondary,
+      altText = 'Exit Gate Card Image',
+      ...props
+    },
+    ref,
+  ) => {
+    const { className: baseClassName, ...commonProps } = getCommonProps(props, 'ExitGateCard');
+    return (
+      <article {...commonProps} className={classnames(baseClassName, className)} {...props} ref={ref}>
+        {imageSrc ? (
+          <SeldonImage
+            objectFit="cover"
+            aspectRatio="16/9"
+            src={imageSrc}
+            alt={altText}
+            className={`${baseClassName}__desktop_image`}
+          />
+        ) : null}
+
+        <div className={`${baseClassName}__content`}>
+          {label ? (
+            <Text variant={TextVariants.body2} element="span" className={`${baseClassName}__content-label`}>
+              {label}
+            </Text>
+          ) : null}
+
+          {header ? (
+            <Text variant={TextVariants.heading1} element="span" className={`${baseClassName}__content-title`}>
+              {header}
+            </Text>
+          ) : null}
+
+          {description ? (
+            <Text variant={TextVariants.body2} element="span" className={`${baseClassName}__content-description`}>
+              {description}
+            </Text>
+          ) : null}
+
+          {(linkLabel || Component) && linkHref ? (
+            <Component href={linkHref}>
+              <Button variant={variant} className={`${baseClassName}__content-link`}>
+                {linkLabel}
+              </Button>
+            </Component>
+          ) : null}
+        </div>
+      </article>
+    );
+  },
+);
+
+ExitGateCard.displayName = 'ExitGateCard';
+
+export default ExitGateCard;
