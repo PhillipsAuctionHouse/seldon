9f65e74a82d675ec336d1433f09cec44d943ce56
Dave McNamara <dmcnamara@phillips.com>
2025-07-29 18:42:24 -0400

    fixing aria warnings

--- src/components/Drawer/Drawer.tsx
+++ src/components/Drawer/Drawer.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { forwardRef } from 'react';
 import { getCommonProps, noOp } from '../../utils';
 import classnames from 'classnames';
 import * as Dialog from '@radix-ui/react-dialog';
 
+import { VisuallyHidden } from '@radix-ui/react-visually-hidden';
 import { DrawerHeader } from './DrawerHeader';
 
 export interface DrawerProps extends React.HTMLAttributes<HTMLDivElement> {
@@ -23,6 +24,11 @@ export interface DrawerProps extends React.HTMLAttributes<HTMLDivElement> {
    * Its presence also triggers the horizontal rule below the header to be rendered.
    */
   headerText?: string;
+  /**
+   * Accessibility label for the drawer, used for screen readers.
+   * Defaults to the headerText if provided, otherwise an empty string.
+   */
+  ariaTitle?: string;
 }
 
 /**
@@ -32,45 +38,49 @@ export interface DrawerProps extends React.HTMLAttributes<HTMLDivElement> {
  *
  */
 
-// TODO this is dumb, why can't vitest see the title within DrawerHeader?
-const isVitest = typeof process !== 'undefined' && process.env.VITEST;
+const Drawer = forwardRef<HTMLDivElement, DrawerProps>(
+  (
+    {
+      className,
+      isOpen = false,
+      onClose = noOp,
+      headerText,
+      title,
+      ariaTitle = headerText ?? title ?? '',
+      children,
+      ...props
+    },
+    ref,
+  ) => {
+    const { className: baseClassName, ...commonProps } = getCommonProps(props, 'Drawer');
 
-const Drawer = ({ className, isOpen = false, onClose = noOp, headerText, children, ...props }: DrawerProps) => {
-  const { className: baseClassName, ...commonProps } = getCommonProps(props, 'Drawer');
+    return (
+      <Dialog.Root
+        open={isOpen}
+        onOpenChange={(open) => {
+          if (!open) {
+            onClose();
+          }
+        }}
+      >
+        <Dialog.Portal>
+          <Dialog.Overlay onClick={onClose} className={`${baseClassName}__overlay`} data-testid="drawer-overlay" />
+          <Dialog.Content className={classnames(baseClassName, className)} id={props.id} {...commonProps} ref={ref}>
+            <VisuallyHidden asChild>
+              <Dialog.Title>{ariaTitle}</Dialog.Title>
+            </VisuallyHidden>
+            <VisuallyHidden asChild>
+              <Dialog.Description>{ariaTitle}</Dialog.Description>
+            </VisuallyHidden>
+            <DrawerHeader baseClassName={baseClassName} headerText={ariaTitle} onClose={onClose} />
+            <div className={`${baseClassName}__content-children`}>{children}</div>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    );
+  },
+);
 
-  const descriptionId = headerText ? 'description-id' : undefined;
-
-  return (
-    <Dialog.Root
-      open={isOpen}
-      onOpenChange={(open) => {
-        if (!open) {
-          onClose();
-        }
-      }}
-    >
-      {isVitest && <Dialog.Title style={{ display: 'none' }} />}
-      <Dialog.Portal>
-        <Dialog.Overlay onClick={onClose} className={`${baseClassName}__overlay`} data-testid="drawer-overlay" />
-        <Dialog.Content
-          className={classnames(baseClassName, className)}
-          id={props.id}
-          aria-describedby={descriptionId}
-          {...commonProps}
-        >
-          <DrawerHeader baseClassName={baseClassName} headerText={headerText} onClose={onClose} />
-
-          {headerText && (
-            <Dialog.Description id={descriptionId} style={{ display: 'none' }}>
-              {headerText}
-            </Dialog.Description>
-          )}
-
-          <div className={`${baseClassName}__content-children`}>{children}</div>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
-  );
-};
+Drawer.displayName = 'Drawer';
 
 export default Drawer;
--- src/components/Drawer/DrawerHeader.tsx
+++ src/components/Drawer/DrawerHeader.tsx
@@ -1,12 +1,9 @@
-import { ComponentProps, forwardRef } from 'react';
+import { ComponentProps } from 'react';
 import * as Dialog from '@radix-ui/react-dialog';
 import IconButton from '../IconButton/IconButton';
 import { ButtonVariants } from '../Button/types';
 import { Icon } from '../Icon';
-import { getCommonProps } from '../../utils';
-
-const withDisplayName = <T, Props>(displayName: string, component: React.ForwardRefRenderFunction<T, Props>) =>
-  Object.assign(forwardRef(component), { displayName });
+import { getCommonProps, px } from '../../utils';
 
 type BaseClassName = { baseClassName: string };
 type DrawerHeaderHelperProps = ComponentProps<'div'> & BaseClassName;
@@ -15,38 +12,29 @@ type DrawerHeaderProps = BaseClassName & {
   onClose: () => void;
 };
 
-const DrawerHeaderContainer = withDisplayName<HTMLDivElement, DrawerHeaderHelperProps>(
-  'DrawerHeaderContainer',
-  ({ baseClassName, children, ...rest }, ref) => (
-    <div className={baseClassName} ref={ref} {...rest}>
-      {children}
-    </div>
-  ),
+const DrawerHeaderContainer = ({ baseClassName, children, ...rest }: DrawerHeaderHelperProps) => (
+  <div className={baseClassName} {...rest}>
+    {children}
+  </div>
 );
 
-const DrawerHeaderBookend = withDisplayName<HTMLDivElement, DrawerHeaderHelperProps>(
-  'DrawerHeaderBookend',
-  ({ baseClassName, children, ...rest }, ref) => (
-    <div className={`${baseClassName}__bookend`} ref={ref} {...rest}>
-      {children}
-    </div>
-  ),
+const DrawerHeaderBookend = ({ baseClassName, children, ...rest }: DrawerHeaderHelperProps) => (
+  <div className={`${baseClassName}__bookend`} {...rest}>
+    {children}
+  </div>
 );
 
-const DrawerHeaderHorizontalRule = withDisplayName<HTMLDivElement, DrawerHeaderHelperProps>(
-  'DrawerHorizontalRule',
-  ({ baseClassName, children, ...rest }, ref) => <div className={`${baseClassName}__hr`} ref={ref} {...rest} />,
+const DrawerHeaderHorizontalRule = ({ baseClassName, children, ...rest }: DrawerHeaderHelperProps) => (
+  <div className={`${baseClassName}__hr`} {...rest} />
 );
 
 const DrawerHeaderBase = ({ baseClassName, headerText, onClose, ...props }: DrawerHeaderProps) => {
-  const { className, ...commonProps } = getCommonProps(props, `${baseClassName.replace('seldon-', '')}-header`);
+  const { className, ...commonProps } = getCommonProps(props, `${baseClassName.replace(px, '')}-header`);
   return (
     <>
       <DrawerHeaderContainer baseClassName={className} {...commonProps}>
         <DrawerHeaderBookend baseClassName={className} />
-        <Dialog.Title className={`${className}__title`} id={className}>
-          {headerText}
-        </Dialog.Title>
+        <p className={`${className}__title`}>{headerText}</p>
         <DrawerHeaderBookend baseClassName={className}>
           <Dialog.Close asChild>
             <IconButton
@@ -65,4 +53,9 @@ const DrawerHeaderBase = ({ baseClassName, headerText, onClose, ...props }: Draw
   );
 };
 
-export const DrawerHeader = withDisplayName<React.ReactNode, DrawerHeaderProps>('DrawerHeader', DrawerHeaderBase);
+DrawerHeaderContainer.displayName = 'DrawerHeaderContainer';
+DrawerHeaderBookend.displayName = 'DrawerHeaderBookend';
+DrawerHeaderHorizontalRule.displayName = 'DrawerHeaderHorizontalRule';
+DrawerHeaderBase.displayName = 'DrawerHeader';
+
+export const DrawerHeader = DrawerHeaderBase;
