f272deeb132231940641675924a6f578f608992e
Zhenting <zhentzhou@gmail.com>
2025-08-07 15:27:24 -0400

    feat(AnnouncementBanner): add

---
+++ src/components/NotificationBanner/NotificationBanner.tsx
@@ -0,0 +1,38 @@
+import classnames from 'classnames';
+import { ComponentProps, forwardRef } from 'react';
+import { getCommonProps } from '../../utils';
+
+// You'll need to change the ComponentProps<"htmlelementname"> to match the top-level element of your component
+export type NotificationBannerProps = ComponentProps<'div'> & {
+  /**
+   * The content to display inside the notification banner.
+   */
+  children: React.ReactNode;
+  /**
+   * A unique `id` for the `<NotificationBanner>`
+   */
+  id: string;
+};
+/**
+ * ## Overview
+ *
+ * Overview of this widget
+ *
+ * [Figma Link](https://www.figma.com/design/qEd5aXjQeiboeJ8GJMOJba/Global-Notification-Banner?node-id=1-1899&m=dev)
+ *
+ * [Storybook Link](https://phillips-seldon.netlify.app/?path=/docs/components-notificationbanner--overview)
+ */
+const NotificationBanner = forwardRef<HTMLDivElement, NotificationBannerProps>(({ className, ...props }, ref) => {
+  const { className: baseClassName, ...commonProps } = getCommonProps(props, 'NotificationBanner');
+  const { children } = props;
+
+  return (
+    <div {...commonProps} className={classnames(baseClassName, className)} {...props} ref={ref}>
+      {children}
+    </div>
+  );
+});
+
+NotificationBanner.displayName = 'NotificationBanner';
+
+export default NotificationBanner;
---
+++ src/components/NotificationBanner/index.ts
@@ -0,0 +1,1 @@
+export { default as NotificationBanner, type NotificationBannerProps } from './NotificationBanner';
