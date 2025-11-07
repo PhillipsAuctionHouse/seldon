f891e24b533c42b2bb6b164f23b70bf3ec7e4053
smarks8 <staceyamarks@gmail.com>
2025-11-07 16:15:47 -0500

    chore: export new type

--- src/patterns/SaleCard/index.ts
+++ src/patterns/SaleCard/index.ts
@@ -1,3 +1,3 @@
 export { default as SaleCard, type SaleCardProps } from './SaleCard';
 export { default as SaleCardActions, type SaleCardActionsProps } from './SaleCardActions';
-export { SaleCardVariants } from './types';
+export { SaleCardVariants, SaleCardImageDisplay } from './types';
