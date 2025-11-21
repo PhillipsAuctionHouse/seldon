2e9dc4e917502ef35348fe045d6e0a794a332ef2
Dave McNamara <dmcnamara@phillips.com>
2025-11-21 15:58:01 -0500

    remove robot comment

--- src/components/Input/utils.ts
+++ src/components/Input/utils.ts
@@ -1,16 +1,14 @@
 import { px } from '../..';
 
-// 🤖 Utility function to get class names for Input component parts,
-// 🤖 created for input-like components to reuse consistent class names.
 export const getInputClassNames = () =>
   ({
+    adornment: `${px}-input__wrapper__adornment`,
+    emptyValidation: `${px}-input__empty-validation`,
+    input: `${px}-input__input`,
     label: `${px}-input__label`,
     labelHidden: `${px}-input__label--hidden`,
-    adornment: `${px}-input__wrapper__adornment`,
     skeleton: `${px}-skeleton`,
-    wrapper: `${px}-input__wrapper`,
-    input: `${px}-input__input`,
-    wrapperInput: `${px}-input__wrapper__input`,
-    emptyValidation: `${px}-input__empty-validation`,
     validation: `${px}-input__validation`,
+    wrapper: `${px}-input__wrapper`,
+    wrapperInput: `${px}-input__wrapper__input`,
   }) as const;
